5e5848d493725fea54d5cedae18db8f1ae5c99e8
Amir <amir.goldental@gmail.com>
2021-01-17 15:59:57 +0200

    update kill switch

--- src/matlab/+mrr/join_as_worker.m
+++ src/matlab/+mrr/join_as_worker.m
@@ -30,7 +30,7 @@ end
 if ishandle(Hndl)
     close(Hndl)
 end
-mrr.redis_cmd(['HSET ' worker_key ' status dead'])
+mrr.redis_cmd(['HSET ' worker_key ' status kill'])
 
     function preform_task(worker_key)
         task_key = mrr.redis_cmd('RPOPLPUSH pending_matlab_tasks ongoing_matlab_tasks');
