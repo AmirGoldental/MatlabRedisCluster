b60d7f2e4636f92124ed9f651506ea7d27c956f0
GumJum <gumjum.o.o@gmail.com>
2021-03-02 08:24:23 +0200

    Update start_matlab_worker.bat
    actually now closes #21

--- src/matlab/start_matlab_worker.bat
+++ src/matlab/start_matlab_worker.bat
@@ -9,6 +9,8 @@ set params_path=%1
 call :logger INFO load parameters from %params_path%
 for /f "tokens=1,* delims==" %%x in ('type "%params_path%"') do call :run_and_set %%x %%y
 
+for /f "usebackq" %%i IN (`hostname`) DO SET hostname=%%i
+
 set cur_dir=%~dp0
 set cur_dir=!cur_dir:~0,-1!
 
@@ -19,7 +21,15 @@ set cur_dir=!cur_dir:~0,-1!
 :: pause
 :: exit /s
 :: :matlab_ok
-start "%random%_matlab_worker" "%matlab_path%" -sd "%cur_dir%" -r "mrc.join_as_worker"
+
+call :send_redis incr workers_count
+if "!res!" == "failed" (
+    call :logger ERROR could not communicate with redis server failed on incr workers_count
+) else (
+    set worker_id=!res!
+    call :send_redis hmset worker:!worker_id! status initializing current_task None last_command None computer %hostname%
+    start "%random%_matlab_worker" "%matlab_path%" -sd "%cur_dir%" -r "mrc.join_as_worker('!worker_id!')"
+)
 timeout /t 30
 exit /s
 
@@ -30,4 +40,10 @@ exit /b
 :logger
 for /f "tokens=1,* delims= " %%a in ("%*") do set ALL_BUT_FIRST=%%b
 echo [%1] %date%T%time% %ALL_BUT_FIRST%
+exit /b
+
+:send_redis
+set res=failed
+set "redis_cmd=%redis_cli_path% -h %redis_hostname% -p %redis_port% -a %redis_password% -n %redis_db%"
+for /f "tokens=*" %%g in ('!redis_cmd! %*') do (set res=%%g)
 exit /b
