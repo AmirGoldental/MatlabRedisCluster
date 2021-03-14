8206c0cd1a1aa51f83b4e0adae296cb2b2f6a241
Amir <Amir.goldental@gmail.com>
2021-03-14 20:24:30 +0200

    close #52

--- src/matlab/+mrc/get_cluster_status.m
+++ src/matlab/+mrc/get_cluster_status.m
@@ -1,15 +1,16 @@
 function cluster_status = get_cluster_status()
 numeric_stats =  mrc.redis_cmd({'LLEN pending_tasks', ...
-    'LLEN ongoing_tasks', 'LLEN finished_tasks', 'LLEN failed_tasks', 'GET tasks_count', 'info'});
+    'LLEN ongoing_tasks', 'LLEN finished_tasks', 'LLEN failed_tasks', 'LLEN pre_pending_tasks', 'GET tasks_count', 'info'});
 
 cluster_status.num_pending = redis_str2double(numeric_stats{1});
 cluster_status.num_ongoing = redis_str2double(numeric_stats{2});
 cluster_status.num_finished = redis_str2double(numeric_stats{3});
 cluster_status.num_failed = redis_str2double(numeric_stats{4});
-cluster_status.num_tasks = redis_str2double(numeric_stats{5});
+cluster_status.num_pre_pending = redis_str2double(numeric_stats{5});
+cluster_status.num_tasks = redis_str2double(numeric_stats{6});
 cluster_status.num_workers = redis_str2double(mrc.redis_cmd('GET workers_count')); 
 
-redis_uptime = redis_str2double(numeric_stats{6});
+redis_uptime = redis_str2double(numeric_stats{7});
 redis_uptime(1: (strfind(redis_uptime, 'uptime_in_seconds') + length('uptime_in_seconds'))) = [];
 redis_uptime((find(redis_uptime == newline, 1)-1):end) = [];
 redis_uptime = str2double(redis_uptime);
