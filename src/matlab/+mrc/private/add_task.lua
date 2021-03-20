38dc7f75a99c8ac85308e1e21834045480828c5e
Amir <Amir.goldental@gmail.com>
2021-03-20 21:04:55 +0200

    fix bug

--- src/matlab/+mrc/private/add_task.lua
+++ src/matlab/+mrc/private/add_task.lua
@@ -2,8 +2,9 @@ local task_id = redis.call('INCR','tasks_count');
 local task_key = 'task:' .. task_id;
 local task_status; 
 for idx=1,#ARGV,1 do 
-    if redis.call('HGET', ARGV[idx], 'status') == 'pending' or redis.call('HGET', ARGV[idx], 'status') == 'pre_pending' or redis.call('HGET', ARGV[idx], 'status') == 'ongoing' then 
-        redis.call('LPUSH', task_key .. ':dependencies', ARGV[idx]); redis.call('LPUSH', ARGV[idx] .. ':prior_to', task_key);
+    if redis.call('HGET', ARGV[idx], 'status') ~= 'finished' then 
+        redis.call('LPUSH', task_key .. ':dependencies', ARGV[idx]); 
+        redis.call('LPUSH', ARGV[idx] .. ':prior_to', task_key);
     end; 
 end; 
 if #ARGV == 0 then 
@@ -15,5 +16,6 @@ else redis.call('RPUSH', 'pre_pending_tasks', task_key); task_status = 'pre_pend
 end;
 redis.call('HMSET', task_key, 'key', task_key, 'id', tostring(task_id),
     'command', KEYS[1], 'created_by', KEYS[2], 'created_on', KEYS[3],
-    'path2add', KEYS[4], 'status', task_status, 'fail_policy', KEYS[5]); 
+    'path2add', KEYS[4], 'status', task_status, 'fail_policy', KEYS[5],
+    'worker', 'None'); 
 return task_key
