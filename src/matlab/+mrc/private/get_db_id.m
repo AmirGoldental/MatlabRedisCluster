42f79fd847396ef631ec82a9cddd3593ade9acdb
Amir <Amir.goldental@gmail.com>
2021-03-22 19:38:10 +0200

    improve db_id

--- src/matlab/+mrc/private/get_db_id.m
+++ src/matlab/+mrc/private/get_db_id.m
@@ -3,7 +3,7 @@ function db_id = get_db_id()
 db_id = mrc.redis_cmd('get db_id');
 while isempty(db_id)
     % DB is empty.
-    db_id = char(randi([uint8('A') uint8('Z')], 1, 32));
+    db_id = datestr(datetime, 'YYYY_mm_dd__HH_MM_SS_FFF');
     mrc.redis_cmd(['setnx db_id ' db_id]);
     db_id = mrc.redis_cmd('get db_id');
 end
