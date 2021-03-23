d6e51d5f5b1f8abc954aac3093869ad9e7f7bc3b
Amir <Amir.goldental@gmail.com>
2021-03-23 14:10:52 +0200

    close #32

--- src/matlab/+mrc/get_cluster_status.m
+++ src/matlab/+mrc/get_cluster_status.m
@@ -1,6 +1,6 @@
 function cluster_status = get_cluster_status()
 numeric_stats =  mrc.redis_cmd({'LLEN pending_tasks', ...
-    'LLEN ongoing_tasks', 'LLEN finished_tasks', 'LLEN failed_tasks', 'LLEN pre_pending_tasks', 'GET tasks_count', 'info'});
+    'LLEN ongoing_tasks', 'LLEN finished_tasks', 'LLEN failed_tasks', 'LLEN pre_pending_tasks', 'GET tasks_count'});
 
 cluster_status.num_pending = redis_str2double(numeric_stats{1});
 cluster_status.num_ongoing = redis_str2double(numeric_stats{2});
@@ -10,16 +10,13 @@ cluster_status.num_pre_pending = redis_str2double(numeric_stats{5});
 cluster_status.num_tasks = redis_str2double(numeric_stats{6});
 cluster_status.num_workers = redis_str2double(mrc.redis_cmd('GET workers_count')); 
 
-redis_uptime = redis_str2double(numeric_stats{7});
-redis_uptime(1: (strfind(redis_uptime, 'uptime_in_seconds') + length('uptime_in_seconds'))) = [];
-redis_uptime((find(redis_uptime == newline, 1)-1):end) = [];
-redis_uptime = str2double(redis_uptime);
-if redis_uptime > 3600*24
-    redis_uptime = [num2str(redis_uptime/(3600*24), 3) 'd'];
-elseif redis_uptime > 3600
-    redis_uptime = [num2str(redis_uptime/3600, 3) 'h'];
+redis_uptime = (now - datenum(get_db_timetag(), 'YYYY_mm_dd__HH_MM_SS_FFF'))*24*60;
+if redis_uptime > 60*24
+    redis_uptime = [num2str(redis_uptime/(60*24), 3) ' d'];
+elseif redis_uptime > 60
+    redis_uptime = [num2str(redis_uptime/60, 3) ' h'];
 else
-    redis_uptime = [num2str(redis_uptime/60, 3) 'm'];
+    redis_uptime = [num2str(redis_uptime, 3) ' m'];
 end    
 cluster_status.uptime = redis_uptime;
 end
