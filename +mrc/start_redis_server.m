3734e735a1a68a8ee8b3f2da11a1eee10c2457c2
Amir <Amir.goldental@gmail.com>
2021-03-27 13:43:42 +0300

    close #76

--- +mrc/start_redis_server.m
+++ +mrc/start_redis_server.m
@@ -1,4 +1,4 @@
 function start_redis_server()
-system(['start "redis_server" /D "' fileparts(fileparts(mfilename('fullpath'))) '" start_redis_server.bat']);
+system(['start "redis_server" /D "' fileparts(fileparts(mfilename('fullpath'))) '" cmd /c start_redis_server.bat']);
 end
 
